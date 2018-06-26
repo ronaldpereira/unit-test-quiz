7859ff4b3e1bf88d14d3a3135154a804b4b68266
Luiz0tavio <me@luizotav.io>
2018-06-25 23:25:03 -0300

    checkNextPreviousButtons() method

--- app/src/androidTest/java/br/ufmg/dcc/unit_test_quiz/Matchers.java
+++ app/src/androidTest/java/br/ufmg/dcc/unit_test_quiz/Matchers.java
@@ -1,11 +1,17 @@
 package br.ufmg.dcc.unit_test_quiz;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 
 class Matchers {
     public static Matcher<View> withListSize (final int size) {
@@ -19,4 +25,27 @@ class Matchers {
             }
         };
     }
+
+    public static String getText(final Matcher<View> matcher) {
+        final String[] stringHolder = { null };
+        onView(matcher).perform(new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextView tv = (TextView)view; //Save, because of check in getConstraints()
+                stringHolder[0] = tv.getText().toString();
+            }
+        });
+        return stringHolder[0];
+    }
+
 }
--- app/src/androidTest/java/br/ufmg/dcc/unit_test_quiz/QuestionsActivityTest.java
+++ app/src/androidTest/java/br/ufmg/dcc/unit_test_quiz/QuestionsActivityTest.java
@@ -1,7 +1,9 @@
 package br.ufmg.dcc.unit_test_quiz;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.rule.ActivityTestRule;
 
@@ -9,6 +11,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import dcc.ufmg.br.quizdetestesunidade.R;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -20,15 +33,35 @@ import static org.junit.Assert.*;
 public class QuestionsActivityTest {
 
     @Rule
-    public final ActivityTestRule<QuestionsActivity> questionsActivityRule =
-            new ActivityTestRule<>(QuestionsActivity.class);
+    public final ActivityTestRule<ModulesActivity> modulesActivityRule = new ActivityTestRule<>(ModulesActivity.class);
 
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    public void checkNextPreviousButtons() {
 
-        assertEquals("br.ufmg.dcc.unit_test_quiz", appContext.getPackageName());
+        int jsonSize = Util.getAssetJsonSize();
+
+        if (jsonSize == 0) {
+            fail();
+        }
+
+        for (int j = 0; j < jsonSize; j++) {
+
+            if(j == 0 || j == 7) continue;
+
+            modulesActivityRule.launchActivity(new Intent());
+
+            onData(anything()).inAdapterView(withId(R.id.activities_list_view)).atPosition(j).perform(click());
+
+            String beforeState = Matchers.getText(withId(R.id.question_number_text_view));
+
+            onView(withId(R.id.next_button)).perform(click());
+            onView(withId(R.id.previous_button)).perform(click());
+
+            String afterState = Matchers.getText(withId(R.id.question_number_text_view));
+
+            assertEquals(beforeState, afterState);
+        }
+
     }
 
 }
